a25e61d0c37317ff1ee43b3b7a4fbe30767984bc
jusansil <juliane-sansil@hotmail.com>
2019-02-06 18:17:45 -0300

    config cors

--- index.js
+++ index.js
@@ -41,7 +41,24 @@ mongoose.connect(config.uri,  { useNewUrlParser: true }, (err) => {
 // ROUTER SERVICE
 
 app.use((req, res) => res.sendFile(INDEX) )
-app.use(cors('Access-Control-Allow-Origin', '*'));
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(logger('dev'));
