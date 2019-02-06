d40d3e75a6107043ae861d8f4a28b132ae4e194b
jusansil <juliane-sansil@hotmail.com>
2019-02-06 17:36:34 -0300

    config cors

--- index.js
+++ index.js
@@ -38,8 +38,11 @@ mongoose.connect(config.uri,  { useNewUrlParser: true }, (err) => {
 // ROUTER SERVICE
 
 app.use((req, res) => res.sendFile(INDEX) )
+app.use(cors('Access-Control-Allow-Origin', '*'));
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(logger('dev'));
 
-app.use(cors ('Access-Controll-Allow-Origin', '*'));
 
 app.use(bodyParser.json());
 
