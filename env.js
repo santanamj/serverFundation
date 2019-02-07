4ee366666aa567a2e735f4c6f35b46aa0e3ee8df
jusansil <juliane-sansil@hotmail.com>
2019-02-06 22:21:23 -0300

    first commit

--- env.js
+++ env.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 
 if (fs.existsSync('./public')) {
   process.env.NODE_ENV = 'production';
-  process.env.databaseUri = 'mongodb://patel:patel@ds153752.mlab.com:53752/angular-2-app'; // Databse URI and database name
-  process.env.databaseName = 'production database: angular-2-app'; // Database name
+  process.env.databaseUri = 'mongodb://msantana:0211ms11d4@ds018258.mlab.com:18258/burguer'; // Databse URI and database name
+  process.env.databaseName = 'production database: burguer-service'; // Database name
 } else {
   process.env.NODE_ENV = 'development';
   process.env.databaseUri = 'mongodb://msantana:0211ms11d4@ds018258.mlab.com:18258/burguer'; // Databse URI and database name
-  process.env.databaseName = 'development burguer-service'; // Database name
+  process.env.databaseName = 'development database: burguer-service'; // Database name
 }
